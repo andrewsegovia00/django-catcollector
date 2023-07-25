b047a74f19aa10b73b9b3290d55f131587c83ae3
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-07-25 15:55:30 -0700

    End of Cat Collector off to Pokemon TCG Collector

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,27 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# Usually a Model is used
+cats = [
+    {"name": "Lolo", "breed": "tabby", "description": "furry little demon", "age": 3},
+    {"name": "Sachi", "breed": "calico", "description": "gentle and loving", "age": 2},
+]
+
 
 def home(request):
     # Include an .html file extension - unlike when rendering EJS templates
     return render(request, "home.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def cats_index(request):
+    return render(
+        request,
+        "cats/index.html",
+        {
+            "cats": cats,
+        },
+    )
