598935e1be8cc1d1e73cc2d6288962228a0f2214
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-07-28 09:40:13 -0700

    Overrode the forms from CBV, added customized forms add attack model with a pk for Pokemon for a one to many relationship

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
+from .forms import FeedingForm
 
 
 # Create your views here.
@@ -29,11 +30,15 @@ def cats_index(request):
 # second parameter is the same name as the variable name in the route which was cat_id
 def cats_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
+    # instantiate FeedingForm to be rendered in the template
+    feeding_form = FeedingForm()
     return render(
         request,
         "cats/detail.html",
         {
+            # include the cat and feeding_form in the context
             "cat": cat,
+            "feeding_form": feeding_form,
         },
     )
 
@@ -51,3 +56,17 @@ class CatUpdate(UpdateView):
 class CatDelete(DeleteView):
     model = Cat
     success_url = "/cats/"
+
+
+# add this new function below cats_detail
+def add_feeding(request, cat_id):
+    # create a ModelForm instance using the data in request.POST
+    form = FeedingForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # don't save the form to the db until it
+        # has the cat_id assigned
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect("detail", cat_id=cat_id)
